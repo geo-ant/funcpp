3746722d11900ac1b7fa5b3572339849e89faa1f
geo-ant <54497890+geo-ant@users.noreply.github.com>
2022-08-18 22:14:30 +0200

    finish testing comptime if for types

--- test/test_type_selector.cpp
+++ test/test_type_selector.cpp
@@ -1,9 +1,9 @@
 // we don't need catch here because we can test everything with static assertions
 #include <type_traits>
-
 #include "funcpp/type_selector.hpp"
 
 using namespace funcpp::type;
+
 
 // a random use case
 constexpr int i = 2;
@@ -25,10 +25,26 @@ static_assert(std::is_same<B3,bool>::value);
 using B4 = If<false>::Then<float>::ElseIf<false>::Then<bool>::Else<int>;
 static_assert(std::is_same<B4,int>::value);
 
+struct dummy{};
 
+using C1 = If<true>::Then<int>::ElseIf<false>::Then<float>::ElseIf<false>::Then<bool>::Else<dummy>;
+static_assert(std::is_same<C1,int>::value);
+using C2 = If<false>::Then<int>::ElseIf<true>::Then<float>::ElseIf<false>::Then<bool>::Else<dummy>;
+static_assert(std::is_same<C2,float>::value);
+using C3 = If<false>::Then<int>::ElseIf<false>::Then<float>::ElseIf<true>::Then<bool>::Else<dummy>;
+static_assert(std::is_same<C3,bool>::value);
+using C4 = If<true>::Then<int>::ElseIf<true>::Then<float>::ElseIf<false>::Then<bool>::Else<dummy>;
+static_assert(std::is_same<C4,int>::value);
+using C5 = If<true>::Then<int>::ElseIf<true>::Then<float>::ElseIf<true>::Then<bool>::Else<dummy>;
+static_assert(std::is_same<C5,int>::value);
+using C6 = If<false>::Then<int>::ElseIf<true>::Then<float>::ElseIf<true>::Then<bool>::Else<dummy>;
+static_assert(std::is_same<C6,float>::value);
+using C7 = If<false>::Then<int>::ElseIf<false>::Then<float>::ElseIf<false>::Then<bool>::Else<dummy>;
+static_assert(std::is_same<C7,dummy>::value);
+using C8 = If<true>::Then<int>::ElseIf<true>::Then<float>::ElseIf<true>::Then<bool>::Else<dummy>;
+static_assert(std::is_same<C8,int>::value);
 
-
+// this is only so that CTest understands that this test passes
 int main() {
-  // this is only so that CTest understands that this test passes
   return 0;
 }
